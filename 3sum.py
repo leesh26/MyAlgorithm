5739c647c8a1173d95e054a31d915aa0d3ebf2b5
leesh26 <74662808+leesh26@users.noreply.github.com>
2021-03-13 12:45:54 +0900

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -5,18 +5,18 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         results = []
         nums.sort()
-		
+	
         for i in range(len(nums)-2):
-						#중복일 시 pass
+	#중복일 시 pass
             if i > 0 and nums[i] == nums[i-1] : 
                 continue
-						# left는 i 다음값, right는 마지막 값
+	# left는 i 다음값, right는 마지막 값
             left, right = i+1, len(nums)-1
             
             while left<right:
                 sum = nums[i]+nums[left]+nums[right]
-                
-								# sum이 0보다 작으면 left을 한칸 오른쪽으로, 0보다 크면 right를 한칸 왼쪽으로 
+             
+		# sum이 0보다 작으면 left을 한칸 오른쪽으로, 0보다 크면 right를 한칸 왼쪽으로 
                 if sum<0:
                     left += 1
                 elif sum>0:
