ee670275ef550466c4206a43398706a5a06f6ee8
unknown <dlthgml1026@naver.com>
2022-06-02 20:49:55 +0900

    220602

--- Java/Baekjoon/B1182.java
+++ Java/Baekjoon/B1182.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B1182 {
+public class Main {
     static int n, s, len;
     static int[] nums;
     static int ans = 0;
